d676ec196d7ef6447367a69894a61145cf5f3d00
Fatima <fxtimaahmed14@gmail.com>
2022-11-18 11:39:08 +0000

    can hire people but there is no hired list

--- src/pages/PersonProfile/components/HireForm.js
+++ src/pages/PersonProfile/components/HireForm.js
@@ -1,10 +1,16 @@
-import { useState } from "react"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function HireForm(props) {
-  const [wage, setWage] = useState(0)
+function HireForm({ person, hiredPeople, setHiredPeople }) {
+  const [wage, setWage] = useState(0);
+  const navigate = useNavigate();
 
   function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
+    const newHire = { ...person, wage: wage };
+    const newPeople = [...hiredPeople, newHire];
+    setHiredPeople(newPeople);
+    navigate("/");
   }
 
   return (
@@ -14,12 +20,12 @@ function HireForm(props) {
         type="text"
         id="wage"
         name="wage"
-        onChange={e => setWage(e.target.value)}
+        onChange={(e) => setWage(e.target.value)}
         value={wage}
       />
       <button type="submit">Hire</button>
     </form>
-  )
+  );
 }
 
-export default HireForm
+export default HireForm;
--- src/pages/PersonProfile/index.js
+++ src/pages/PersonProfile/index.js
@@ -1,19 +1,32 @@
-import { useState } from "react"
-import HireForm from "./components/HireForm"
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-function PersonProfile(props) {
-  const [person, setPerson] = useState(null)
+import HireForm from "./components/HireForm";
 
-  if (!person) return <p>Loading...</p>
+function PersonProfile({ hiredPeople, setHiredPeople }) {
+  const [person, setPerson] = useState(null);
+
+  const location = useLocation();
+  useEffect(() => {
+    if (!location.state) return;
+    const personToHireData = location.state.person;
+    setPerson(personToHireData);
+  }, [location]);
+
+  if (!person) return <p>Loading...</p>;
 
   return (
     <article>
       <h2>
         {person.name.first} {person.name.last}
       </h2>
-      <HireForm person={person} />
+      <HireForm
+        person={person}
+        hiredPeople={hiredPeople}
+        setHiredPeople={setHiredPeople}
+      />
     </article>
-  )
+  );
 }
 
-export default PersonProfile
+export default PersonProfile;
